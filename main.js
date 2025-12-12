dc6511accb4b074f0adbdd2223ebaa7280503845
Audrey Shi <audrey.shi108@gmail.com>
2025-12-12 14:10:09 +1300

    Added style to the landing page, added javascript so that the page would redirect to the designated card after inputting correct code.

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
 function redirect() {
     const button = document.getElementById("submitButton");
     const form = document.getElementById("Form");
+    const status = document.getElementById("status");
     var InputCode = document.getElementById("Code")
-    console.log(InputCode)
+    console.log(InputCode.value)
+    if (InputCode.value === "2561") {
+        status.textContent = "Success! Redirecting...";
+        document.location.href = "card1.html";
+    } else {
+        status.textContent = "Invalid code, please try again.";
+    }
+        
 }
