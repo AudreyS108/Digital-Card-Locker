c95a3a5bbc9a7a93d2e3cd5fb8e79a50504e702c
Audrey Shi <audrey.shi108@gmail.com>
2025-12-17 17:29:31 +1300

    Added a 2025 xmas card. Added more assets

--- card-xmas-2025/xmas2025a/a2025.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const card = document.getElementById("card");
-  const audio = document.getElementById("audio");
-
-  card.addEventListener("mouseenter", function() {
-    audio.play()
-  })
-});
